65b82521bcb581bc9e02635d6d24ec50078cece9
dustin1216 <dk121692@gmail.com>
2017-04-27 08:00:35 -0400

    Add files via upload

---
+++ SRC/eGFX/Sprites/Sprites_16BPP_565.h
@@ -0,0 +1,15 @@
+#include "stdint.h" 
+#include "eGFX.h" 
+
+#ifndef _SPRITES_16BPP_565_H
+#define _SPRITES_16BPP_565_H
+
+////***************************************************************************
+////                         Sprite_16BPP_565_badge
+////***************************************************************************
+
+
+extern const eGFX_ImagePlane 	Sprite_16BPP_565_badge;
+
+#endif
+
